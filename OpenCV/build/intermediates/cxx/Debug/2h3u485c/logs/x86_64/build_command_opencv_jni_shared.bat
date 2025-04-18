189556c921cd2523cca773df19faa98d5c664434
snaeb11 <suisui.vincent@gmail.com>
2025-04-18 18:09:48 +0800

    amoghus

---
+++ OpenCV/build/intermediates/cxx/Debug/2h3u485c/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\aglya\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\aglya\\Desktop\\signitup2\\OpenCV\\.cxx\\Debug\\2h3u485c\\x86_64" ^
+  opencv_jni_shared
---
+++ OpenCV/build/intermediates/cxx/Debug/2h3u485c/logs/x86_64/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\aglya\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\aglya\\Desktop\\signitup2\\OpenCV\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86_64" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86_64" ^
+  "-DANDROID_NDK=C:\\Users\\aglya\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\aglya\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\aglya\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\aglya\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\aglya\\Desktop\\signitup2\\OpenCV\\build\\intermediates\\cxx\\Debug\\2h3u485c\\obj\\x86_64" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\aglya\\Desktop\\signitup2\\OpenCV\\build\\intermediates\\cxx\\Debug\\2h3u485c\\obj\\x86_64" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\aglya\\Desktop\\signitup2\\OpenCV\\.cxx\\Debug\\2h3u485c\\x86_64" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
